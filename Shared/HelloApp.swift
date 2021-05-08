b97f96a5263d794d9d584be55dd54a197fb17ef4
ybzhao <ybzhao@sifli.com>
2021-05-08 14:12:12 +0800

    1st commit

---
+++ Shared/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Shared
+//
+//  Created by Sifli on 2021/5/8.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
